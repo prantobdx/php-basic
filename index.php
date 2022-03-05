efc1bb267576254193e9f3d3568c8b4d46ea6f9c
prantobdx <pranto35-1858@diu.edu.bd>
2022-03-05 12:08:40 +0600

    project-w-14_15

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use App\classes\Example;
+
+$example = new Example();
+$example->index();
+
+
+
+/* 3 major rules for variable naming
+   * start with $ sign
+   * A-Z, a-z, 0-9, _, ()
+   * no number in first
+   
+   ** standard rules for variable naming.
+
+   *start with small letter
+   *meaningful 
+   *readable
+*/
+
+echo '</br>';
+$arif = 10;
+echo $arif;
+  
